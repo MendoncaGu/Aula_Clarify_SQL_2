b4dc06be61f90d42fd94009ae549f142fb7a1281
MendoncaGu <gugamendocaduarte2@gmail.com>
2025-10-15 22:01:14 -0300

    2ª Aula

--- 02_insert_variaveis_avancado.sql
+++ 02_insert_variaveis_avancado.sql
@@ -24,4 +24,75 @@ DECLARE @valor_total DECIMAL (10,2); -- Valor do total do pedido
 DECLARE @data_venda DATETIME = GETDATE(); -- Data Atual
 DECLARE @status_transacao VARCHAR(50); --
 
-SELECT @
+-- calcular o valor da venda
+SELECT @valor_total = p.preco * @quantidade
+FROM produtos p 
+WHERE p.produto_id = @produto_id;
+
+-- validação para garantir que a quantidade seja vendida
+IF @quantidade <= 0
+BEGIN
+	SET @status_transacao = 'Falha: Quantidade invalida';
+	ROLLBACK TRANSACTION; -- Reverte a transação caso a quantidade seja invalida
+	PRINT @status_transacao;
+	RETURN;
+END
+
+-- Inserindo outra venda usando nosso novo 'metodo'
+INSERT INTO vendas
+	(cliente_id, produto_id, quantidade,valor_total,data_venda)
+VALUES
+	(@cliente_id, @produto_id, @quantidade, @valor_total, @data_venda)
+
+IF @@ERROR <> 0
+BEGIN
+	SET @status_transacao = 'Falha: Erro na inserção da venda';
+	ROLLBACK TRANSACTION;
+	PRINT @status_transacao;
+	RETURN;
+END
+
+-- Se todas as inserções forem ok, confirma a transacao
+SET @status_transacao = 'Sucesso: Vendas inseridas com sucesso!'
+
+COMMIT TRANSACTION;
+
+SELECT * FROM vendas
+
+/*=====================================================
+					Caso erro
+
+=====================================================*/
+BEGIN TRANSACTION;
+
+
+DECLARE @cliente_id INT = 1; --Cliente para o pedido (caio)
+DECLARE @produto_id INT = 2; --Produto Comprado (Notebook)
+DECLARE @quantidade INT = 3; --Quantidade comprada (3 Unidades)
+DECLARE @valor_total DECIMAL (10,2); -- Valor do total do pedido
+DECLARE @data_venda DATETIME = GETDATE(); -- Data Atual
+DECLARE @status_transacao VARCHAR(50); --
+
+SET @quantidade = -1;
+SET @cliente_id = 1;
+SET @produto_id = 1;
+SET @data_venda = GETDATE();
+
+SELECT @valor_total = p.preco * @quantidade
+FROM produtos p 
+WHERE p.produto_id = @produto_id;
+
+IF @quantidade <= 0
+BEGIN
+	SET @status_transacao = 'Falha: Quantidade invalida';
+	ROLLBACK TRANSACTION; -- Reverte a transação caso a quantidade seja invalida
+	PRINT @status_transacao;
+	RETURN;
+END
+
+INSERT INTO vendas
+	(cliente_id, produto_id, quantidade,valor_total,data_venda)
+VALUES
+	(@cliente_id, @produto_id, @quantidade, @valor_total, @data_venda)
+
+COMMIT TRANSACTION
