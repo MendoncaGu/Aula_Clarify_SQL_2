e2dd5af27015cbb6fc3de0c521e2628a46e42fd6
MendoncaGu <gugamendocaduarte2@gmail.com>
2025-10-15 22:12:16 -0300

    2ª Aula

---
+++ 05_batch_insert_otimizacao.sql
@@ -0,0 +1,25 @@
+
+
+/*
+Este script demonstra como realizar inserções em lote
+utilizando as transações.
+Ou seja, inserir um grande volume de dados de forma
+eficiente dividindo em pequenos lotes
+('batches' ou 'chunks')
+*/
+
+USE db1410_empresaMuitoLegal
+GO
+
+CREATE TABLE vendas (
+	vendas_id INT IDENTITY (1,1) PRIMARY KEY,
+	cliente_id INT,
+	produto_id INT,
+	quantidade INT,
+	valor_total DECIMAL (10,2),
+	data_venda DATETIME
+);
+
+-- Variaveis para o controle de lotes
+DECLARE @batch_size INT = 1000;
+DECLARE @total_registros INT = 10000;
