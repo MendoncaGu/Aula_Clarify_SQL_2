67413b044740bcaa75b407b83de643d30fcbaa27
MendoncaGu <gugamendocaduarte2@gmail.com>
2025-10-17 21:59:15 -0300

    4ª Aula

--- 08_Delete_eficiente.sql
+++ 08_Delete_eficiente.sql
@@ -39,5 +39,55 @@ FROM master.dbo.spt_values a , master.dbo.spt_values b;
 --EXECUTADO DAQUI--
 
 --Truncate remove todas as linhas da tabela, mais eficiente que o delete pois não registra a remoção das linhas
-TRUNCATE TABLE clientes;
+--TRUNCATE TABLE clientes;
 
+INSERT INTO pedidos(pedido_id, cliente_id, data_pedido, valor_total)
+SELECT TOP 100000
+	ROW_NUMBER() OVER(ORDER BY (SELECT NULL)),
+	(ABS(CHECKSUM(NEWID())) % 100000) + 1,
+	DATEADD(DAY, -(ROW_NUMBER() OVER( ORDER BY (SELECT NULL)) % 3650),
+	GETDATE()
+	),
+	CAST(RAND() * 1000 AS DECIMAL (10,2))
+	FROM master.dbo.spt_values a, master.dbo.spt_values b;
+
+SELECT TOP 10 * FROM pedidos
+SELECT TOP 10 * FROM clientes
+
+SELECT COUNT(*) FROM pedidos
+SELECT COUNT(*) FROM clientes
+
+BEGIN TRY
+	BEGIN TRANSACTION
+	-- decalrando as variaveis de controle do lote
+		DECLARE @batch_size INT = 1000;
+		DECLARE @row_count INT;
+		-- inicializando a variavel de controle da contagem de registro exclusivos
+		SET @row_count = 1
+		--looping para excluir os dados em lote
+		WHILE @row_count > 0
+		BEGIN
+			--excluir os dados em lotes de 1000
+			DELETE TOP (@batch_size)
+			FROM clientes
+			WHERE data_cadastro < DATEADD(YEAR, -5, GETDATE());
+			--Obtendo a contagem de registros na iteração atual
+			SET @row_count = @@ROWCOUNT;
+			--Exibindo o progresso
+			PRINT 'Excluidos' + CAST(@row_count AS VARCHAR) + 'Registros de clientes'
+				
+			--esperar 1 segundo entre lotes, visando evitar blocks
+			--por parte do servidor
+			WAITFOR DELAY '00:00:01';
+
+
+			END
+	COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+IF @@TRANCOUNT > 0
+	BEGIN
+		ROLLBACK TRANSACTION;
+	END
+	PRINT 'Erro na exclusão: ' + ERROR_MESSAGE(); 
+END CATCH;
---
+++ 09_alter_avançada.sql
@@ -0,0 +1,59 @@
+
+
+CREATE DATABASE db1410_alterAvancada;
+GO
+
+USE db1410_alterAvancada;
+GO
+
+DROP TABLE IF EXISTS clientes
+CREATE TABLE clientes(
+	cliente_id INT,
+	nome_cliente VARCHAR(100),
+	data_cadastro DATETIME
+	/*
+	Em resumo: "crie uma restrição chamada PK_clientes_cliente_id
+	que torne a coluna cliente_id a chave primaria da tabela"
+	Isso grante a unicidade e identificação exclusiva de cada cliente
+	na tabela cliente
+	*/
+	CONSTRAINT PK_clientes_cliente_id PRIMARY KEY(cliente_id)
+);
+
+INSERT INTO clientes
+(cliente_id, nome_cliente, data_cadastro)
+VALUES
+(1, 'Caio', '2025-01-02'),
+(2, 'Rodrigo', '2025-01-03'),
+(3, 'Rafael', '2025-01-04'),
+(4, 'Gusatvo', '2025-01-05');
+
+-- como remover uma CONSTRANT
+-- passo 01: Remover chave primaria existente
+ALTER TABLE clientes
+DROP CONSTRAINT PK_clientes_cliente_id;
+
+--passo 02: Adicionar uma nova chave 
+ALTER TABLE clientes
+ADD CONSTRAINT PK_clientes_cliente_id_2 PRIMARY KEY (cliente_id);
+
+--Adicionando um indice para otimizar a consulta
+--por data nesse exemplo
+
+CREATE NONCLUSTERED INDEX IX_clientes_data_cadastro ON clientes(data_cadastro);
+
+-- Alterar um tipo
+ALTER TABLE clientes 
+ALTER COLUMN nome_cliente TEXT;
+
+-- Adicionando uma nova coluna 
+ALTER TABLE clientes
+ADD email_cliente VARCHAR(150)
+
+--Verificar se o indice existe na tabela
+SELECT * FROM sys.indexes WHERE name = 'IX_clientes_data_cadastro';
+
+--Funcionamento
+ALTER TABLE clientes
+DROP CONSTRAINT PK_clientes_cliente_id_2
+
